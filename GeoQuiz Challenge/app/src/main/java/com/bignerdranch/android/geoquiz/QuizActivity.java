2b2367454e173b538fffe21ea4d338160908c609
Пользователь <zditovetskaya.d@yandex.ru>
2018-06-10 12:37:57 +0300

    add saveInstanceState

--- GeoQuiz Challenge/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ GeoQuiz Challenge/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -13,6 +13,8 @@ import android.widget.Toast;
 public class QuizActivity extends AppCompatActivity {
 
     private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+
     private Button mTrueButton;
     private Button mFalseButton;
     private ImageButton mNextButton;
@@ -95,7 +97,17 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+        if(savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
         updateQuestion();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
     }
 
     @Override
